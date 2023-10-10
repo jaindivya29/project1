3f79bb5c94f0500bf538b40796a61e307b3d5346
djain08 <divyaa.jainn29@gmail.com>
2023-10-10 21:01:28 +0530

    webdev

---
+++ readmore.js
@@ -0,0 +1,9 @@
+function openPost(){
+    window.open('post1.html','_blank');
+    window.open('post2.html','_blank');
+    window.open('post3.html','_blank');
+    window.open('post4.html','_blank');
+
+}
+
+document.querySelector('.button').addEventListener('click',openPost)
